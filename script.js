8d9a8dba939381d7c513712b9ac9cce2501c67cf
Laís de Souza Neves 2 D <116578290+usersoryu00@users.noreply.github.com>
2024-07-30 12:19:48 -0300

    script.js

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ const perguntas = [
         enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
+                texto: "Isso é espantoso!",
                 afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
             },
             {
-                texto: "Isso é maravilhoso!",
+                texto: "Isso é magnífico!",
                 afirmacao: "Quis saber como usar IA no seu dia a dia."
             }
         ]
